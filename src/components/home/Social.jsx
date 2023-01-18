3f67fda8e0af1a9b5a498703c621c2d837e3658f
Jose de la Ossa C <joseandresdelaossa@gmail.com>
2023-01-17 21:55:02 -0500

    add about me

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href='https://www.instagram.com/jose_a_carmona/' className='home__social-icon' target="_black">
+            <i className='uil uil-instagram'></i>
+        </a>
+
+        <a href='https://www.linkedin.com/in/jose-andres-de-la-ossa-carmona-790251226/' className='home__social-icon' target="_black">
+            <i className='uil uil-dribbble'></i>
+        </a>
+
+        <a href='https://github.com/JoseCarmona17' className='home__social-icon' target="_black">
+            <i className='uil uil-github-alt'></i>
+        </a>
+
+    </div>
+  )
+}
