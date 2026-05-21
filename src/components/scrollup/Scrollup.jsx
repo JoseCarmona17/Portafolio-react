297cc3777e43023ca2630bcac8dd60a9c72704e9
Jose de la Ossa C <Joseandresdelaossa@gmail.com>
2026-05-21 09:12:11 -0500

    update scrollup

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -13,7 +13,7 @@ export const ScrollUp = () => {
     });
 
   return (
-    <a href="#" className="scrollup">
+    <a href="#home" className="scrollup">
         <i className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   )
